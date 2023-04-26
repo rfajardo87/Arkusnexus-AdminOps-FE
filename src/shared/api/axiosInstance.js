29f75240f4c5378a0ee1cc0569fe14cbf8f4c14b
rfajardo87 <rfaiard@gmail.com>
2023-04-26 17:40:17 -0600

    solicitdes backend con token

--- src/shared/api/axiosInstance.js
+++ src/shared/api/axiosInstance.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookie from "js-cookie";
 import { env } from "./env.local";
 
 export const axiosInstance = (token) =>
@@ -6,6 +7,7 @@ export const axiosInstance = (token) =>
     baseURL: `${env.protocol}://${env.url}:${env.port}`,
     headers: {
       "Acess-Control-Allow-Origin": "*",
+      Authorization: `Bearer ${Cookie.get("token")}`,
     },
     token,
   });
